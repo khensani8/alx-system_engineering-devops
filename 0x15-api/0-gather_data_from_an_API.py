420a2a6340c873d7c656d200edf539f626599acb
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2024-01-29 20:10:37 +0200

    fixes

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,39 +4,22 @@ import requests
 import sys
 
 
-def get_employee_todo_progress(employee_id):
-    try:
-        """Fetch user information"""
-        user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-        user_response = requests.get(user_url)
-        user_data = user_response.json()
-        employee_name = user_data.get('name')
-
-        """Fetch tasks information"""
-        tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-        tasks_response = requests.get(tasks_url)
-        tasks_data = tasks_response.json()
-
-        """Count completed tasks"""
-        completed_tasks = [task for task in tasks_data if task.get('completed')]
-        num_completed_tasks = len(completed_tasks)
-        total_tasks = len(tasks_data)
-
-        """Display progress information"""
-        print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
-        
-        """Display titles of completed tasks"""
-        for task in completed_tasks:
-            print(f"{' ' * 5}{task.get('title')}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        sys.exit(1)
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    json_o = res.json()
+    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
 
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            l_task.append(task)
+
+    print("({}/{}):".format(len(l_task), len(tasks)))
+    for task in l_task:
+        print("\t {}".format(task.get("title")))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,38 +1,34 @@
 #!/usr/bin/python3
 """ Script that uses JSONPlaceholder API to get information about employee """
-import riequests
 import csv
+import requests
 import sys
 
-def get_employee_todo_progress(employee_id):
-    try:
-        user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-        user_response = requests.get(user_url)
-        user_data = user_response.json()
-        employee_name = user_data.get('name')
-
-        tasks_url = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-        tasks_response = requests.get(tasks_url)
-        tasks_data = tasks_response.json()
-
-        csv_filename = f'{employee_id}.csv'
-        with open(csv_filename, mode='w', newline='') as csv_file:
-            csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-            csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-            for task in tasks_data:
-                csv_writer.writerow([employee_id, employee_name, task.get('completed'), task.get('title')])
-
-        print(f"Data exported to {csv_filename}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        sys.exit(1)
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    employee_id = int(sys.argv[1])
-    get_employee_todo_progress(employee_id)
+    userid = sys.argv[1]
+    user = '{}users/{}'.format(url, userid)
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
 
+    todos = '{}todos?userId={}'.format(url, userid)
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
+    for task in tasks:
+        l_task.append([userid,
+                       name,
+                       task.get('completed'),
+                       task.get('title')])
+
+    filename = '{}.csv'.format(userid)
+    with open(filename, mode='w') as employee_file:
+        employee_writer = csv.writer(employee_file,
+                                     delimiter=',',
+                                     quotechar='"',
+                                     quoting=csv.QUOTE_ALL)
+        for task in l_task:
+            employee_writer.writerow(task)
