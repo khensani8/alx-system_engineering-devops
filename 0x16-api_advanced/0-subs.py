7f29379082927b2c9a5684af7832e5cad611efac
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2024-02-13 16:51:26 +0200

    fixes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""Task 0 - a function queries the Reddit API and returns number of subs
-    for a given subreddit
-"""
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-     """Queries the Reddit API and returns the number of subscribers
+    """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
-     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-     headers = {"User-Agent": "YesYourBot"}
-     response = requests.get(url, headers=headers)
+    import requests
 
-     if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-     
-     else:
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
-     
+
+    return sub_info.json().get("data").get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,19 @@
-#!/usr/bin/python3 
-"""function that queries the Reddit API and prints the titles of the first 10 hot posts listed 
-for a given subreddit
-"""
-import requests
+#!/usr/bin/python3
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-     """checks the top ten in subreddit"""
-     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-     headers = {"User-Agent": "YourBotName"}
-     response = requests.get(url, headers=headers)
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
-     if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
-        else:
-            print("None")
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
+    else:
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
+        
