79f48608533937b1775b593b12f12987517f7df8
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2024-02-13 17:06:41 +0200

    number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """function that queries the Reddit API and returns the 
-    number of subscribers"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "YourBotName"}  # Set a custom User-Agent
-    response = requests.get(url, headers=headers)
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
     else:
-        return 0
+        print(None)
