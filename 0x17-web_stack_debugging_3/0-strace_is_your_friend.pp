3743afa57bccb2880eb8de26c0d36aa187bc64a2
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2024-02-15 05:31:43 +0200

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
-# Creat a manifest that fix all termintion of phpp.
-
-exec { 'fix_phpp':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+# Fixes a wordpress site running on apache2
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
